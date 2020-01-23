c5bf4582fa604ef9db3bfe2bc7d3f5770aa78747
neerajvashistha <neerajvash8@gmail.com>
2020-01-23 10:08:26 +0000

    clean up

--- Lab/NLP/Project (copy)/Classes/DenseTransformer.py
+++
@@ -1,9 +0,0 @@
-from sklearn.base import TransformerMixin
-
-class DenseTransformer(TransformerMixin):
-
-    def fit(self, X, y=None, **fit_params):
-        return self
-
-    def transform(self, X, y=None, **fit_params):
-        return X.todense()
--- Lab/NLP/Project (copy)/Helper/DebugPrint.py
+++
@@ -1,5 +0,0 @@
-debug_on = False
-
-def DebugPrint(debug_string):
-    if debug_on:
-        print(debug_string)
